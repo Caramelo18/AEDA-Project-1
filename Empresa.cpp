96027fbe7718b3ee6691a08578bdb4fe429f1ae3
Fábio Caramelo <fabiocaramelo18@gmail.com>
2015-10-13 16:05:51 +0100

    Class Empresa created

---
+++ Empresa.cpp
@@ -0,0 +1,12 @@
+/*
+ * Empresa.cpp
+ *
+ *  Created on: 13/10/2015
+ *      Author: fabio
+ */
+#include "Empresa.h"
+
+
+
+
+
---
+++ Empresa.h
@@ -0,0 +1,29 @@
+/*
+ * Empresa.h
+ *
+ *  Created on: 13/10/2015
+ *      Author: fabio
+ */
+
+#ifndef EMPRESA_H_
+#define EMPRESA_H_
+
+#include <vector>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Empresa
+{
+private:
+	vector<int> camioes;
+	vector<int> servicos;
+	vector<int> clientes;
+public:
+	vector<int> getCamioes();
+};
+
+
+
+#endif /* EMPRESA_H_ */
